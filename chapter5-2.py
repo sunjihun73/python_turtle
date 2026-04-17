ad8f7515655c4f1521b6f6ad6f3d43c966cfcbf0
sunjeehun73 <sunjeehun@dbinc.co.kr>
2026-04-17 21:17:02 +0900

    거북이 만나기 시뮬 파일 추가

---
+++ chapter5-2.py
@@ -0,0 +1,18 @@
+# import
+import turtle
+
+
+# 함수
+def main(pName):
+  fruit = ["사과", '배', '딸기', '포도']
+  fruit.append("귤")
+  print(fruit)
+
+  if "딸기" not in fruit:
+    print("딸기가 없네요!")
+
+
+
+# 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
+if __name__ == '__main__':
+  main('sunjihun')
